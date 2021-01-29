1aa381e5be952036f00c0aeb9373de87ffd57017
RosaMontserrat <al221911582@gmail.com>
2021-01-29 16:35:52 -0600

    10 Using a device id for the token name

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:dio/dio.dart' as Dio;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_authentication_with_laravel_sanctum1/models/user.dart';
+import 'package:platform_device_id/platform_device_id.dart';
 
 import '../dio.dart';
 
@@ -16,10 +16,10 @@ class Auth extends ChangeNotifier {
   User get user => _user;
 
   Future login ({ Map credentials }) async {
-
+    String deviceId = await getDeviceId();
     Dio.Response response = await dio().post(
       'auth/token',
-      data: json.encode(credentials)
+      data: json.encode(credentials..addAll({ 'deviceId': deviceId}))
     );
 
     String token = json.decode(response.toString())['token'];
@@ -45,6 +45,19 @@ class Auth extends ChangeNotifier {
       _authenticated = false;
     }
     notifyListeners();
+  }
+
+  Future getDeviceId () async{
+    String deviceId;
+
+    try{
+      deviceId = await PlatformDeviceId.getDeviceId;
+    }catch (e){
+      //
+    }
+
+    return deviceId;
+
   }
 
   void logout () {
